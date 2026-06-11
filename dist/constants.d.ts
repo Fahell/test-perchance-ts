1bde37a3f44dbea8268cf30f0789cf3910ff4453
Fahell <32997538+Fahell@users.noreply.github.com>
2026-06-10 22:02:25 -0300

    chore: release v1.0.8 (#10)
    * fix: use window.root instead of window.perchance for Perchance bridge
    
    * chore: release v1.0.8

--- dist/constants.d.ts
+++ dist/constants.d.ts
@@ -1,4 +1,4 @@
-export declare const VERSION = "v1.0.7";
-export declare const CDN_BASE = "https://cdn.jsdelivr.net/gh/Fahell/test-perchance-ts@v1.0.7";
-export declare const BUNDLE_PATH = "https://cdn.jsdelivr.net/gh/Fahell/test-perchance-ts@v1.0.7/dist/main.bundle.js";
+export declare const VERSION = "v1.0.8";
+export declare const CDN_BASE = "https://cdn.jsdelivr.net/gh/Fahell/test-perchance-ts@v1.0.8";
+export declare const BUNDLE_PATH = "https://cdn.jsdelivr.net/gh/Fahell/test-perchance-ts@v1.0.8/dist/main.bundle.js";
 //# sourceMappingURL=constants.d.ts.map
